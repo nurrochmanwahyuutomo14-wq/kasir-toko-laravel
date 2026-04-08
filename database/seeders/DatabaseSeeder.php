4a536c7f6ffa0a4044fbbc2a1b74e94576d67c34
Nur Rochman <nurrochmanwahyuutomo14@gmail.com>
2026-04-08 15:02:12 +0700

    Fix DatabaseSeeder untuk akun Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Membuat akun admin secara manual tanpa fitur Faker
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'], // Login pakai email ini
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password123'), // Ini passwordnya: 12345678 (Bisa Mas Nur ganti)
+            ]
+        );
     }
 }
